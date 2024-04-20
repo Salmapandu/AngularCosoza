912d2384f89583ad035ff57d06b03638d52660f8
Salmapandu <salmapandu281@gmail.com>
2024-04-20 06:26:28 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+
 
 @Component({
   selector: 'app-root',
