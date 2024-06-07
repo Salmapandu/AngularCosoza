b66fcaeb4c1c890b4b8b57f47c731a5633617093
Ramadhanmkoma <ramadhanmkoma9@gmail.com>
2024-06-07 16:44:24 +0300

    data...

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,19 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideAnimationsAsync(), BrowserAnimationsModule]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideAnimationsAsync(),
+    provideToastr({
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+      timeOut: 3000,
+    }),
+    BrowserAnimationsModule,
+  ],
 };
