962b335e67b1982a933a7d47e3f1dfd224ec0a18
Ramadhanmkoma <ramadhanmkoma9@gmail.com>
2024-07-14 14:50:41 +0300

    art work route implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,102 +1,83 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
+import { ListAddEditComponent } from './list-add-edit/list-add-edit.component';
+import { PageAddEditComponent } from './page-add-edit/page-add-edit.component';
+import { ArtworkComponent } from './pages/artwork/artwork.component';
+import { ConfigurationComponent } from './pages/configuration/configuration.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LayoutComponent } from './pages/layout/layout.component';
-import path from 'path';
-import { Component } from '@angular/core';
-import { report } from 'process';
+import { LoginComponent } from './pages/login/login.component';
+import { UserListComponent } from './pages/user-list/user-list.component';
 import { UserManageComponent } from './pages/user-manage/user-manage.component';
 import { UserReportComponent } from './pages/user-report/user-report.component';
-import { UserLoyaltyComponent } from './pages/user-loyalty/user-loyalty.component';
-import { ConfigurationComponent } from './pages/configuration/configuration.component';
-import { UserListComponent } from './pages/user-list/user-list.component';
-import { PageAddEditComponent } from './page-add-edit/page-add-edit.component';
-import { ListAddEditComponent } from './list-add-edit/list-add-edit.component';
-
-
-
-
 
 export const routes: Routes = [
-    {
-        path:'', redirectTo: 'login', pathMatch:'full'
-        
-    },
-    {
-        path: 'login',
-        component:LoginComponent
-    },
-    {
-        path:'app',
-        component:LayoutComponent,
-        children:[
-            {
-                path: '',
-                redirectTo: 'dashboard',
-                pathMatch: 'full'   
-            },
-            {
-                path:'dashboard',
-                loadComponent: () => DashboardComponent
-            },
-            {
-                path:'user_report',
-                loadComponent: () => UserReportComponent, 
-                 
-            },
-            {
-                path: 'user-manage',
-                loadComponent: () => UserManageComponent,
-                children:[
-                    {
-                        path: '',
-                        redirectTo: 'page-add-edit',
-                        pathMatch: 'full'
-                    },
-                ]
-                
-            },
-            {
-                path:'page-add-edit',
-                loadComponent: () => PageAddEditComponent, 
-                 
-            },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'app',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () => DashboardComponent,
+      },
+      {
+        path: 'user_report',
+        loadComponent: () => UserReportComponent,
+      },
+      {
+        path: 'user-manage',
+        loadComponent: () => UserManageComponent,
+        children: [
+          {
+            path: '',
+            redirectTo: 'page-add-edit',
+            pathMatch: 'full',
+          },
+        ],
+      },
+      {
+        path: 'page-add-edit',
+        loadComponent: () => PageAddEditComponent,
+      },
+      {
+        path: 'artwork',
+        loadComponent: () => ArtworkComponent,
+        title: 'ArtWorks'
+      },
+      {
+        path: 'user-list',
+        loadComponent: () => UserListComponent,
+        children: [
+          {
+            path: '',
+            redirectTo: 'list-add-edit',
+            pathMatch: 'full',
+          },
+        ],
+      },
 
-        
-            {
-                path: 'user-list',
-                loadComponent: () => UserListComponent,
-                children:[
-                    {
-                        path: '',
-                        redirectTo: 'list-add-edit',
-                        pathMatch: 'full'
-                    },
-                ]
-                
-            },
+      {
+        path: 'list-add-edit',
+        loadComponent: () => ListAddEditComponent,
+      },
 
-            {
-                path: 'list-add-edit',
-                loadComponent: () => ListAddEditComponent,
-            },
-
-
-           
-            {
-                path: 'configuration',
-                loadComponent: () => ConfigurationComponent,
-              
-            },
-            
-
-
-    
-        ]
-
-        
-          
-
-    },
-
+      {
+        path: 'configuration',
+        loadComponent: () => ConfigurationComponent,
+      },
+    ],
+  },
 ];
